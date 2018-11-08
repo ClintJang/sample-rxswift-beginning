0e62c2b4152a996a74151cf2c034fff5f3ed76cb
Clint Jang <jang.wangsu@gmail.com>
2018-11-08 18:34:30 +0900

    Fix : 첫번째 셈플로 이동하는 기능 추가, items 수정

--- JWSRxSwiftBeginningSample/JWSRxSwiftBeginningSample/ViewController.swift
+++ JWSRxSwiftBeginningSample/JWSRxSwiftBeginningSample/ViewController.swift
@@ -8,6 +8,21 @@
 
 import UIKit
 
+enum MainCellItem: String, CaseIterable {
+    case row1 = "구독자에게 알리는 방법의 이해"
+    case row2 = "셈플 소스 두번째 아직 입니다."
+    case row3 = "아직 입니다."
+
+    init?(id : Int) {
+        switch id {
+        case 1: self = .row1
+        case 2: self = .row2
+        case 3: self = .row3
+        default: return nil
+        }
+    }
+}
+
 class ViewController: BaseViewController {
 
     @IBOutlet weak var tableView: UITableView!
@@ -16,11 +31,9 @@ class ViewController: BaseViewController {
         super.viewDidLoad()
 
         // 테이블 뷰에 넣을 데이터
-        let items = Observable.just([
-            "구독자에게 알리는 방법의 이해",
-            "셈플 소스 두번째 아직 입니다.",
-            "아직 입니다."
-        ])
+        let items = Observable.just(
+            MainCellItem.allCases.map({ $0.rawValue })
+        )
         
         // 셀
         items
@@ -36,9 +49,26 @@ class ViewController: BaseViewController {
             .modelSelected(String.self)
             .subscribe(onNext:  { value in
                 print("\(value)")
-                UIViewController.showAlert("\(value)")
             })
+            .disposed(by: disposeBag)
+        
+        // 델리게이트 연결
+        tableView.rx.setDelegate(self)
             .disposed(by: disposeBag)
     }
 }
 
+extension ViewController : UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("indexPath:\(indexPath)")
+        
+        if indexPath.row == 0 {
+            // 구독자에게 알리는 방법의 이해
+            let storyboard = UIStoryboard.init(name: "Start", bundle: nil)
+            let nextViewController = storyboard.instantiateViewController(withIdentifier: "StartViewController")
+            self.navigationController?.pushViewController(nextViewController, animated: true)
+        } else {
+            UIViewController.showAlert("준비 중 입니다.")
+        }
+    }
+}
