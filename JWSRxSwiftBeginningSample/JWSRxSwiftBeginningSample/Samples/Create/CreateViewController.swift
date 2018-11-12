30528c70aae2abf778b35f102d72bba0150b8a6c
Clint Jang <jang.wangsu@gmail.com>
2018-11-12 16:34:08 +0900

    Fix : Create()에 대한 고민

--- JWSRxSwiftBeginningSample/JWSRxSwiftBeginningSample/Samples/Create/CreateViewController.swift
+++ JWSRxSwiftBeginningSample/JWSRxSwiftBeginningSample/Samples/Create/CreateViewController.swift
@@ -8,26 +8,91 @@
 
 import UIKit
 
-class CreateViewController: UIViewController {
+enum TestError : Error {
+    case createError
+}
+
+class CreateViewController: BaseViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        print("===============================")
+        
         let _ = Observable<Int>.create { (observer) -> Disposable in
             
             observer.onNext(1)
             observer.onNext(2)
+            observer.onCompleted()
             
             return Disposables.create()
-        }
+            }.subscribe(onNext: { print($0) },
+                        onError: { print("Error:\($0)") },
+                        onCompleted:{ print("onCompleted") } )
+        .disposed(by: disposeBag)
+        
+        print("===============================")
+        
+        let _ = Observable<Int>.create { (observer) -> Disposable in
+            
+            observer.onNext(1)
+            observer.onNext(2)
+            observer.onNext(3)
+
+            do {
+                // 강제 에러 발생
+//                try self.someThrowingFunction()
+                throw TestError.createError
+                
+            } catch {
+                 observer.onError(error)
+            }
+            
+            // onNext(4)와 onCompleted는 실행되지 않음
+            observer.onNext(4)
+            observer.onCompleted()
+            
+            return Disposables.create()
+            }.subscribe(onNext: { print($0) },
+                        onError: { print("Error:\($0)") },
+                        onCompleted:{ print("onCompleted") } )
+            .disposed(by: disposeBag)
+        
+        print("===============================")
+        
+        let _ = Observable<Int>.create { (observer) -> Disposable in
+            
+            [1,2,3,4,5].forEach({
+                observer.onNext($0)
+            })
+            observer.onCompleted()
+            
+            return Disposables.create()
+            }.subscribe(onNext: { print($0) },
+                        onError: { print("Error:\($0)") },
+                        onCompleted:{ print("onCompleted") } )
+        .disposed(by: disposeBag)
+
+        print("===============================")
         
         let _ = Observable<String>.create { (observer) -> Disposable in
             
             observer.onNext("1")
             observer.onNext("2")
+            observer.onCompleted()
             
             return Disposables.create()
-        }
-        
+            }.subscribe(onNext: { print($0) },
+                        onError: { print("Error:\($0)") },
+                        onCompleted:{ print("onCompleted") } )
+        .disposed(by: disposeBag)
     }
+
+    // 불필요
+//    func someThrowingFunction() throws -> Int {
+//        // 강제 에러 발생
+//        throw TestError.createError
+//
+////        return 0
+//    }
 }
