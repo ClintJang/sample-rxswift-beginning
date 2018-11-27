5318ef00a7309e4e4dba32bc2e9fd2d5d6d122b7
Clint Jang <jang.wangsu@gmail.com>
2018-11-27 19:48:54 +0900

    Fix : 구구단 두번째 케이스 추가

--- JWSRxSwiftBeginningSample/JWSRxSwiftBeginningSample/Samples/Gugudan/GugudanViewController.swift
+++ JWSRxSwiftBeginningSample/JWSRxSwiftBeginningSample/Samples/Gugudan/GugudanViewController.swift
@@ -30,8 +30,11 @@ class GugudanViewController: BaseViewController {
             .disposed(by: disposeBag)
         
         
-        // 앱 시작시 콘솔 1단 테스트, 만들기 전에 사전 테스트용도
-        consoleFrom1To9Dan()
+        // 앱 시작시 콘솔 구구단 전부 찍기, 케이스 1
+        consoleFrom1To9DanCase1()
+        
+        // 앱 시작시 콘솔 구구단 전부 찍기, 케이스 2
+//        consoleFrom1To9DanCase2()
     }
         
         
@@ -52,7 +55,7 @@ class GugudanViewController: BaseViewController {
 }
 
 extension GugudanViewController {
-    func consoleFrom1To9Dan() {
+    func consoleFrom1To9DanCase1() {
         print("===============================")
         print("== 콘솔 로그용")
         print("\n\n")
@@ -69,12 +72,31 @@ extension GugudanViewController {
 //            )
 //            .disposed(by: disposeBag)
         
-        // 1부터 9단 찍기
+        // 1부터 9단 찍기, flatMap 활용
         Observable.range(start: 1, count: 9)
             .flatMap { dan in
                 Observable.range(start: 1, count: 9).map({ row in
                     return "\(dan) * \(row) = \(dan * row)\n"
                 }).reduce("\(dan)단 출력\n", accumulator: { "\($0)\($1)" })
+            }
+            .subscribe(onNext:{ print($0)},
+                       onError:{ print($0) },
+                       onCompleted:{ print("onCompleted")}
+            )
+            .disposed(by: disposeBag)
+    }
+    
+    func consoleFrom1To9DanCase2() {
+        print("===============================")
+        print("== 콘솔 로그용")
+        print("\n\n")
+        
+        // 1부터 9단 찍기, concatMap 활용
+        Observable.range(start: 1, count: 9)
+            .concatMap { dan in
+                Observable.range(start: 1, count: 9).map({ row in
+                    return "\(dan) * \(row) = \(dan * row)\n"
+                })
             }
             .subscribe(onNext:{ print($0)},
                        onError:{ print($0) },
